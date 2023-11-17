42487f1ef9d7bd7fee299b91ae9e66824f22d290
Thomas Shank <thomasshankgit@gmail.com>
2023-11-17 14:45:09 -0500

    Added old code from text_to_speech.py

---
+++ encoding/text_to_speech.py
@@ -0,0 +1,31 @@
+# Utilizes the Bark model from HuggingFace
+import torch
+import warnings
+
+from transformers import AutoProcessor, AutoModel
+from scipy.io.wavfile import write
+
+warnings.filterwarnings(action="ignore", category=UserWarning)
+
+
+class TextToSpeech:
+    _filename = "output.wav"
+    _device = "cuda" if torch.cuda.is_available() else "cpu"
+    _voices = {"en": "v2/en_speaker_1"}
+
+    _model_id = "suno/bark"
+    _model = AutoModel.from_pretrained(_model_id).to(_device)
+    _processor = AutoProcessor.from_pretrained(_model_id, voice_preset=_voices["en"])
+
+    def __init__(self) -> None:
+        print("Text-to-speech initialized")
+
+    def synthesize(self, text):
+        inputs = self._processor(text=[text], return_tensors="pt").to(self._device)
+        return self._model.generate(**inputs, do_sample=True, pad_token_id=1337)
+
+    def write(self, audio_array):
+        sample_rate = self._model.generation_config.sample_rate
+        write(
+            self._filename, rate=sample_rate, data=audio_array.cpu().numpy().squeeze()
+        )
